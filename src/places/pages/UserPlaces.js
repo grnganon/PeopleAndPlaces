6608b278c0de365a0bf65824b8d71226e2579fd7
richardhughes <grnganon@gmail.com>
2020-11-15 18:25:03 -0500

    Fix for only using selected user's places

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList/PlaceList';
 
 const DUMMY_PLACES = [
@@ -9,9 +10,7 @@ const DUMMY_PLACES = [
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
     address: '20 W 34th St, New York, NY 10001',
-    location: {      lat: 40.7484405,
-      lng: -73.9878584
-    },
+    location: { lat: 40.7484405, lng: -73.9878584 },
     creator: 'u1'
   },
   {
@@ -30,7 +29,10 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  // userParams will give access to URL parameters, so it is looking for :userId
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
