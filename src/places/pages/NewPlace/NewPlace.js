9f69b499d6d8eb9184953cfabb8d92c0f8607bac
richardhughes <grnganon@gmail.com>
2020-11-21 02:02:09 -0500

    fixed styling

--- src/places/pages/NewPlace/NewPlace.js
+++ src/places/pages/NewPlace/NewPlace.js
@@ -1,12 +1,12 @@
 import React from 'react';
+
+import './NewPlace.css';
 import Input from '../../../shared/components/FormElements/Input/Input';
 
 const NewPlace = () => {
-  // Stubbing out some add place logic,not too deep, will eventually send to server instead
-  // Not worth adding to 'DUMMY_PLACES'
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input element="input" type="text" label="Title" />
     </form>
   );
 };
