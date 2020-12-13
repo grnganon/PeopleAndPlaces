3e50e4fd4701f64bc96a60748ed0f87d20a1828e
richardhughes <grnganon@gmail.com>
2020-12-13 04:11:21 -0500

    Lots of refactor, now updating edit form fill in after data comes in from "back-end".

--- src/places/pages/UpdatePlace/UpdatePlace.js
+++ src/places/pages/UpdatePlace/UpdatePlace.js
@@ -1,43 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { DUMMY_PLACES } from '../UserPlaces/UserPlaces';
+
+import { useForm } from '../../../shared/hooks/form-hook';
+import Button from '../../../shared/components/FormElements/Button/Button';
 import Input from '../../../shared/components/FormElements/Input/Input';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE
 } from '../../../shared/util/validators';
-import Button from '../../../shared/components/FormElements/Button/Button';
-import './UpdatePlace.css';
-import { useForm } from '../../../shared/hooks/form-hook';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
+];
 
 const UpdatePlace = () => {
-  // again using 'useParams' to grab :placeId from the URL
+  const [isLoading, setIsLoading] = useState(true);
   const placeId = useParams().placeId;
-  const identifiedPlace = DUMMY_PLACES.find(place => place.id === placeId);
 
-  const [formState, inputHandler] = useForm(
+  const [formState, inputHandler, setFormData] = useForm(
     {
-      title: { value: identifiedPlace.title, isValid: true },
+      title: {
+        value: '',
+        isValid: false
+      },
       description: {
-        value: identifiedPlace.description,
-        isValid: true
+        value: '',
+        isValid: false
       }
     },
-    true
+    false
   );
 
-  const updatePlaceSubmitHandler = event => {
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true
+        }
+      },
+      true
+    );
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
+
+  const placeUpdateSubmitHandler = event => {
     event.preventDefault();
     console.log(formState.inputs);
   };
 
-  // if place not found in list of places return error message otherwise render form
-  return !identifiedPlace ? (
-    <div className="center">
-      <h2>Could not find place!</h2>
-    </div>
-  ) : (
-    <form className="place-form" onSubmit={updatePlaceSubmitHandler}>
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -54,13 +114,13 @@ const UpdatePlace = () => {
         element="textarea"
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a description with at least 5 characters."
+        errorText="Please enter a valid description (min. 5 characters)."
         onInput={inputHandler}
         initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
       <Button type="submit" disabled={!formState.isValid}>
-        Update Place
+        UPDATE PLACE
       </Button>
     </form>
   );
--- src/shared/hooks/form-hook.js
+++ src/shared/hooks/form-hook.js
@@ -12,17 +12,27 @@ export const useForm = (initialInputs, initialFormValidity) => {
     isValid: initialFormValidity
   });
 
-    const inputHandler = useCallback((id, value, isValid) => {
-        dispatch({
-            type: 'INPUT_CHANGE',
-            value: value,
-            isValid: isValid,
-            inputId: id
-        });
-    }, []);
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id
+    });
+  }, []);
 
-    // Hooks can actually return anything
-    return [formState, inputHandler];
+  // we are now setting default for fields in update place, and want to simulate how we would update once a back-end API call returned with the place info
+  const setFormData = useCallback((inputData, formValidity) => {
+    dispatch({
+      type: 'SET_DATA',
+      inputs: inputData,
+      formIsValid: formValidity
+    });
+  }, []);
+
+  // Hooks can actually return anything
+  // Added setFormData as a third returned element... callback func for updating to initial state from off of the default values
+  return [formState, inputHandler, setFormData];
 };
 
 const formReducer = (state, action) => {
@@ -45,6 +55,8 @@ const formReducer = (state, action) => {
         },
         isValid: formIsValid
       };
+    case 'SET_DATA':
+      return { inputs: action.inputs, isValid: action.formIsValid };
     default:
       return state;
   }
