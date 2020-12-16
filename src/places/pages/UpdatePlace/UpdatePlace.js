489d2169d38460ae9f42b51d7f2f014f846adb68
richardhughes <grnganon@gmail.com>
2020-12-16 00:56:54 -0500

    Re-used some UI components for styling, some error handling. Little fixes

--- src/places/pages/UpdatePlace/UpdatePlace.js
+++ src/places/pages/UpdatePlace/UpdatePlace.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useForm } from '../../../shared/hooks/form-hook';
 import Button from '../../../shared/components/FormElements/Button/Button';
 import Input from '../../../shared/components/FormElements/Input/Input';
+import Card from '../../../shared/components/UIElements/Card/Card';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE
@@ -12,7 +13,7 @@ import {
 const DUMMY_PLACES = [
   {
     id: 'p1',
-    title: 'Empire State Building',
+    title: 'Emp. State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
@@ -59,19 +60,22 @@ const UpdatePlace = () => {
   const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
   useEffect(() => {
-    setFormData(
-      {
-        title: {
-          value: identifiedPlace.title,
-          isValid: true
+    if (identifiedPlace) {
+      setFormData(
+        {
+          title: {
+            value: identifiedPlace.title,
+            isValid: true
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true
+          }
         },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true
-        }
-      },
-      true
-    );
+        true
+      );
+    }
+
     setIsLoading(false);
   }, [setFormData, identifiedPlace]);
 
@@ -83,7 +87,9 @@ const UpdatePlace = () => {
   if (!identifiedPlace) {
     return (
       <div className="center">
-        <h2>Could not find place!</h2>
+        <Card>
+          <h2>Could not find place!</h2>
+        </Card>
       </div>
     );
   }
